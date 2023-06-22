f2d94d6ec59323912ff267063daabc82786250ce
VictorJust <victorjust09@gmail.com>
2023-06-22 14:44:03 +0300

    Add application v0.1

---
+++ FileData.cs
@@ -0,0 +1,10 @@
+﻿
+namespace Directory_Scanner
+{
+    public class FileData
+    {
+        public string FileName { get; set; }
+        public long FileSize { get; set; }
+        public string MimeType { get; set; }
+    }
+}
